a403aeeff2750a407392725b4288156a5b70e2f8
guest271314 <guest271314@gmail.com>
2019-07-19 14:23:04 +0000

    Update native-messaging-mkvmerge.js

--- app/native-messaging-mkvmerge.js
+++ app/native-messaging-mkvmerge.js
@@ -4,7 +4,7 @@
 let [port, fileNames, appendTo, dir, result] = [null, [], "--append-to "];
 const [hostName, mimeType, cmd, options, metadata
   , outputFileName, randomFileName, getTrack] = [
-  "native_messaging_mkvmerge", "video/x-matroska;codecs=vp9,opus"
+  "native_messaging_mkvmerge", "video/webm;codecs=vp9,opus"
   // path to mkvmerge at OS
   , "./mkvmerge", "-o", "-J", "merged.webm"
   , _ => "_" + ".".repeat(16).replace(/./g, _ =>
